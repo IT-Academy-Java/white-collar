51142924a03700412c5d4e6fbe55876e61fad055
Elena Piaggio <elenapiaggio@gmail.com>
2020-10-01 12:38:36 +0200

    ADD Shop bean

--- src/main/java/com/itacademy/whitecollar/dto/ShopResponseDto.java
+++ src/main/java/com/itacademy/whitecollar/dto/ShopResponseDto.java
@@ -1,4 +1,17 @@
 package com.itacademy.whitecollar.dto;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="shops")
 public class ShopResponseDto {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int maxCapacity;
 }
