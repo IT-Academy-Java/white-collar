d72b63fe10ae64b060051e5d3aa3d46ce0805386
Elena Piaggio <elenapiaggio@gmail.com>
2020-11-01 18:07:26 +0100

    update controller

--- src/main/java/com/itacademy/whitecollar/controller/PictureController.java
+++ src/main/java/com/itacademy/whitecollar/controller/PictureController.java
@@ -1,33 +1,33 @@
 package com.itacademy.whitecollar.controller;
 
 import com.itacademy.whitecollar.dto.PictureResponseDto;
-import com.itacademy.whitecollar.repository.IPictureRepository;
+import com.itacademy.whitecollar.service.impl.PictureServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @CrossOrigin(origins = "*", methods= { RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE })
 @RequestMapping("/shops")
 public class PictureController {
 
     @Autowired
-    private IPictureRepository iPictureRepository;
+    private PictureServiceImpl pictureService;
+
 
     @GetMapping("/pictures")
     @ResponseBody
     public List<PictureResponseDto> getAllPictures() throws Exception{
-        return iPictureRepository.findAll();
+        return pictureService.findAll();
     }
 
     @GetMapping("/{id}/pictures")
     @ResponseBody
     public List<PictureResponseDto> getPicturesByShop(@PathVariable(value = "id") Long id) throws Exception{
-        return iPictureRepository.findByShopId(id);
+        return pictureService.findByShopId(id);
     }
 
     @PostMapping("/{id}/pictures")
@@ -35,21 +35,21 @@ public class PictureController {
             @PathVariable(value = "id") Long id,
             @RequestBody PictureResponseDto picture
     ) {
-        iPictureRepository.create(picture, id);
+        pictureService.create(picture, id);
         return new ResponseEntity<>(picture, HttpStatus.OK);
     }
 
     @DeleteMapping("/pictures/{id}")
     public ResponseEntity<PictureResponseDto> delete(@PathVariable(value = "id") Long id) {
-        PictureResponseDto picture = iPictureRepository.findOne(id);
-        iPictureRepository.delete(id);
+        PictureResponseDto picture = pictureService.findOne(id);
+        pictureService.delete(id);
         return new ResponseEntity<>(picture, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}/pictures")
     public ResponseEntity<PictureResponseDto> deletePicturesByShop(@PathVariable(value = "id") Long id) {
-        PictureResponseDto picture = iPictureRepository.findOne(id);
-        iPictureRepository.deleteByShopId(id);
+        PictureResponseDto picture = pictureService.findOne(id);
+        pictureService.deleteByShopId(id);
         return new ResponseEntity<>(picture, HttpStatus.OK);
     }
 }
--- src/main/java/com/itacademy/whitecollar/controller/ShopController.java
+++ src/main/java/com/itacademy/whitecollar/controller/ShopController.java
@@ -1,39 +1,38 @@
 package com.itacademy.whitecollar.controller;
 
 import com.itacademy.whitecollar.dto.ShopResponseDto;
-import com.itacademy.whitecollar.repository.IShopRepository;
+import com.itacademy.whitecollar.service.impl.ShopServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @CrossOrigin(origins = "*", methods= { RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE })
 @RequestMapping("/shops")
 public class ShopController {
 
     @Autowired
-    private IShopRepository iShopRepository;
+    private ShopServiceImpl shopService;
 
     @GetMapping("/")
     @ResponseBody
     public List<ShopResponseDto> getAllShops() throws Exception{
-        return iShopRepository.findAll();
+        return shopService.findAll();
     }
 
     @PostMapping("/")
     public ResponseEntity<ShopResponseDto> save(@RequestBody ShopResponseDto shop) {
-        iShopRepository.create(shop);
+        shopService.create(shop);
         return new ResponseEntity<>(shop, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<ShopResponseDto> delete(@PathVariable(value = "id") Long id) {
-        ShopResponseDto shop = iShopRepository.findOne(id);
-        iShopRepository.delete(id);
+        ShopResponseDto shop = shopService.findOne(id);
+        shopService.delete(id);
         return new ResponseEntity<>(shop, HttpStatus.OK);
     }
 }
